68d0781d9f31614317336a1669134a9c6a36a78d
madwilliam <madwilliam5225@gmail.com>
2022-10-25 16:26:51 -0700

    upd

--- pipeline.py
+++ pipeline.py
@@ -20,12 +20,12 @@ def processs_side_view_data(data_path):
     # shutil.copytree( data_path,destination, ignore=shutil.ignore_patterns('*.avi'),copy_function = shutil.copy)
 
 def processs_top_view_data(data_path):
-    side_view_config_file ='/data/SideviewLeft_Feb2022-Arash-2022-02-08/config.yaml';
-    head_config_file ='/net/dk-server/afassihizakeri/DLC/SC_Muscimol_2019-Arash_2-2022-10-03/config.yaml';
+    top_view_config_file ='\\dk-server.dk.ucsd.edu\afassihizakeri\DLC\WhiskerMuscimol-xx-2022-10-10\config.yaml';
+    head_config_file ='\\dk-server.dk.ucsd.edu\afassihizakeri\DLC\SC_Muscimol_2019-Arash_2-2022-10-03\config.yaml';
     # destination='/net/dk-server/afassihizakeri/SC_Movies/ar32motor/2021_08_02/Side'
 
-    make_movie_and_stimulus_file(data_path,parallel=True,ncores = 16)
-    save_trial_n(data_path)
+    # make_movie_and_stimulus_file(data_path,parallel=True,ncores = 16)
+    # save_trial_n(data_path)
 
     text_files = [os.path.join(data_path,f) for f in os.listdir(data_path) if f.endswith('video.mp4') and not f.endswith('L.avi') and not f.endswith('R.avi') and not f.endswith('videopoints.avi') and not f.endswith('videopoints.avi')]
     deeplabcut.analyze_videos(head_config_file,text_files,shuffle=1, save_as_csv=True )
@@ -35,8 +35,8 @@ def processs_top_view_data(data_path):
 
     XfilesL = [os.path.join(data_path,f) for f in os.listdir(data_path) if f.startswith('Mask')  ] # find all files with R.avi as file name
     XfilesR = [os.path.join(data_path,f) for f in os.listdir(data_path) if f.startswith('Mirror')  ] # find all files with R.avi as file name
-    deeplabcut.analyze_videos(side_view_config_file,XfilesL,shuffle=1, save_as_csv=True)
-    deeplabcut.filterpredictions(side_view_config_file,XfilesL,shuffle=1)
-    deeplabcut.analyze_videos(side_view_config_file,XfilesR,shuffle=1, save_as_csv=True)
-    deeplabcut.filterpredictions(side_view_config_file,XfilesR,shuffle=1)
+    deeplabcut.analyze_videos(top_view_config_file,XfilesL,shuffle=1, save_as_csv=True)
+    deeplabcut.filterpredictions(top_view_config_file,XfilesL,shuffle=1)
+    deeplabcut.analyze_videos(top_view_config_file,XfilesR,shuffle=1, save_as_csv=True)
+    deeplabcut.filterpredictions(top_view_config_file,XfilesR,shuffle=1)
     # shutil.copytree( data_path,destination, ignore=shutil.ignore_patterns('*.avi'),copy_function = shutil.copy)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from pipeline import processs_top_view_data
 
-data_path = '/net/dk-server/afassihizakeri/movies_Rat_SC_project/ar2muscimol500nl500igmar13muscimolleft5ul500nl/10_11_19'
+data_path = r'\\dk-server.dk.ucsd.edu\afassihizakeri\movies_Rat_SC_project\ar2muscimol500nl500igmar13muscimolleft5ul500nl\10_11_19\\'
 processs_top_view_data(data_path)
