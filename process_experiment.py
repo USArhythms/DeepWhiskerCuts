39eb41e63542db507f6e235caffc606a649d1052
madwilliam5225 <madwilliam5225@gmail.com>
2022-11-07 16:39:58 -0800

    upd

--- process_experiment.py
+++ process_experiment.py
@@ -1,12 +1,13 @@
 
 from pipeline import processs_side_view_data
+from setting import data_root
+import os
 import argparse
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    data_path = r'D:\Sidevideos\ar37motor\2022_07_28'
     parser.add_argument("path", type=str,
                     help="data path to annalyze")
     args = parser.parse_args()
-    data_path = args.path
+    data_path = os.path.join(data_root,args.path)
     processs_side_view_data(data_path)
