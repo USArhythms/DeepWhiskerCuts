f2396d3b2abd6c1c4af5b3745e33bdd57bbba574
unknown <dklab@DESKTOP-B6QTE7O.ad.ucsd.edu>
2022-11-10 15:18:02 -0800

    upd

--- list_available_trial_folders.py
+++ list_available_trial_folders.py
@@ -4,7 +4,7 @@ from setting import this_computer
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("folder", type=str,
+    parser.add_argument("--folder", type=str,
                     help="animal folder")
     args = parser.parse_args()
-    print(os.listdir(this_computer['data_path'],args.folder))
+    print(os.listdir(os.path.join(this_computer['data_path'],args.folder)))
