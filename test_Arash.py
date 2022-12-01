08422cdb4f173ddcdfff5d0c8a9fdec7afcff207
madwilliam5225 <madwilliam5225@gmail.com>
2022-12-01 14:32:06 -0800

    upd

---
+++ test_Arash.py
@@ -0,0 +1,4 @@
+from pipeline import processs_top_view_data
+
+data_path = r'\\dk-server.dk.ucsd.edu\afassihizakeri\movies_Rat_SC_project\ar19Muscimolrightside500mg500nl\10_19_19\\'
+processs_top_view_data(data_path)
