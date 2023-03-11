bc0f709788dce609a196de482d8c8a9b7ce1a19f
unknown <dklab@DESKTOP-B6QTE7O.ad.ucsd.edu>
2023-03-10 16:40:47 -0800

    upd

--- test_script.py
+++ test_script.py
@@ -2,12 +2,14 @@ from DeepWhiskerCuts.lib.ProgressManager import ExperimentManager
 from DeepWhiskerCuts.lib.pipeline import analyze_videos
 from DeepWhiskerCuts.setting.dlc_setting import eye_shuffle
 import os
-dir = r'D:\Sidevideos\ar37motor\2023_02_22_ 163949'
+import pdb
+dir = r'D:\Sidevideos\ar37motor\2023_02_22'
 manager = ExperimentManager(dir,'side')
 for triali in manager.trials:
     if not triali.finished:
+        # pdb.set_trace()
         triali.print_progress()
-        eye_videos = [os.path.join(manager.dir,triali.name+'.avi')]
+        eye_videos = [os.path.join(manager.dir,triali.name+'EYE.avi')]
         analyze_videos(eye_videos,'eye_config',shuffle=eye_shuffle)
         print('undone') 
 print('done')
