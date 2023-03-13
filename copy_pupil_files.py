5d43eb53d21bc36fd5c1d4cf4ba09e48524a38a7
unknown <dklab@DESKTOP-B6QTE7O.ad.ucsd.edu>
2023-03-13 13:51:30 -0700

    commit

---
+++ copy_pupil_files.py
@@ -0,0 +1,6 @@
+from DeepWhiskerCuts.lib.ProgressManager import ExperimentManager
+from DeepWhiskerCuts.setting.setting import this_computer
+
+dir = r'D:\Sidevideos\ar37motor\2023_02_22'
+manager = ExperimentManager(dir,mode = 'side')
+manager.copy_files_for_pupil_qc(this_computer['pupil_destination'])
