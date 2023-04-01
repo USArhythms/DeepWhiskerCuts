ccd58f3f29f3d25f1b11cb41c9df652ee795c403
madwilliam5225 <madwilliam5225@gmail.com>
2023-03-31 19:12:39 -0700

    upd

--- run_side_view.py
+++ run_side_view.py
@@ -1,9 +1,7 @@
 from DeepWhiskerCuts.lib.ProgressManager import ExperimentManager
-from DeepWhiskerCuts.lib.pipeline import analyze_videos
-from DeepWhiskerCuts.setting.dlc_setting import eye_shuffle
-import os
-import pdb
-dir = r'C:\sidevideos\ar37motor\2023_02_22'
+from DeepWhiskerCuts.lib.pipeline import processs_side_view_data
+dir = r'C:\sidevideos\ar37\2023_03_23_ 182349'
+processs_side_view_data(dir)
 manager = ExperimentManager(dir,'side')
 for triali in manager.trials:
     if not triali.finished:
