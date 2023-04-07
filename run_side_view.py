fee1327663e4c3c829e12bb7adbfec44b54eb9f9
madwilliam5225 <madwilliam5225@gmail.com>
2023-04-07 13:45:07 -0700

    upd

--- run_side_view.py
+++ run_side_view.py
@@ -1,6 +1,6 @@
 from DeepWhiskerCuts.lib.ProgressManager import ExperimentManager
 from DeepWhiskerCuts.lib.pipeline import processs_side_view_data
-dir = r'C:\sidevideos\ar37\2023_03_23_ 182349'
+dir = r'C:\Sidevideos\ar37\2023_03_23_ 182249'
 processs_side_view_data(dir)
 manager = ExperimentManager(dir,'side')
 for triali in manager.trials:
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from pipeline import *
 from DeepWhiskerCuts.lib.top_view_spliter import split_left_and_right_from_top_video
 
-data_path = r'F:\videos\ar37motor\2023_02_22_ 163923'
+data_path = r'\\dk-server.dk.ucsd.edu\data\afassihizakeri\topview data\2023_03_23_ 182302'
 
+# analyze_top_view_video(data_path)
+# split_left_and_right_from_top_video(data_path)
+# analyze_left_video(data_path)
 analyze_top_view_video(data_path)
-split_left_and_right_from_top_video(data_path)
-analyze_left_video(data_path)
-analyze_right_video(data_path)
