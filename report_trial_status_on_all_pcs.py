f07220ce2b337d41213efabc153e9d7c45ffa619
madwilliam5225 <madwilliam5225@gmail.com>
2023-04-21 13:42:03 -0700

    upd

--- report_trial_status_on_all_pcs.py
+++ report_trial_status_on_all_pcs.py
@@ -1,16 +1,10 @@
 
-from DeepWhiskerCuts.setting.setting import common_cache,this_computer,computers
-from DeepWhiskerCuts.lib.StatusMonitor import StatusMonitor,load_common_trials
-# from DeepWhiskerCuts.lib.remote_utility import run_python_script,pick_folder
+from DeepWhiskerCuts.setting.setting import common_cache,computers
+from DeepWhiskerCuts.lib.StatusMonitor import load_common_trials
 from DeepWhiskerCuts.lib.remote_utility import pick_folder
 import pickle
 import os
 import pdb
-
-# monitor = StatusMonitor()
-# for computer in computers:
-#     pdb.set_trace()
-#     run_python_script(computer,'find_status.py')
 
 common_trials = load_common_trials()
 files = os.listdir(common_cache)
@@ -21,13 +15,13 @@ for computer in computers:
     status[computer] = pickle.load(open(os.path.join(common_cache,f'{computer}.status'),'rb'))
 
 animals = list(common_trials.keys())
-for animal in common_trials:
-    trials = common_trials[animal]
-    for triali in trials:
-        for computer in computers:
-            try:
-                print(f'computer: {computer}')
-                manager = status[computer][animal][triali]
-                manager.print_progress()
-            except:
-                pdb.set_trace()
+animal = pick_folder(animals,'pick an animal')
+trials = common_trials[animal]
+triali = pick_folder(list(trials),'pick an animal')
+for computer in computers:
+    try:
+        print(f'computer: {computer}')
+        manager = status[computer][animal][triali]
+        manager.print_progress()
+    except:
+        pdb.set_trace()
